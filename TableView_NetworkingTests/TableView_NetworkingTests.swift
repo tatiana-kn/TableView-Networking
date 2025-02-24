83f3434d6236c3ebaefcf4c60569fb919f429b2f
tatiana-kn <tv.kniazeva@gmail.com>
2025-02-24 15:29:01 +0400

    add unit tests

---
+++ TableView_NetworkingTests/TableView_NetworkingTests.swift
@@ -0,0 +1,36 @@
+//
+//  TableView_NetworkingTests.swift
+//  TableView+NetworkingTests
+//
+//  Created by Tia M on 2/22/25.
+//
+
+import XCTest
+@testable import TableView_Networking
+
+class TableView_NetworkingTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Поместите сюда настройку кода.
+              // Этот метод вызывается перед вызовом каждого тестового метода в классе.
+    }
+
+    override func tearDownWithError() throws {
+        // Поместите сюда код разрушения.
+              // Этот метод вызывается после вызова каждого тестового метода в классе.
+    }
+
+    func testExample() throws {
+        // Это пример функционального тестового случая.
+        // Используйте XCTAssert и связанные функции для проверки того,
+        // что тесты дают правильные результаты.
+    }
+
+    func testPerformanceExample() throws {
+        // Это пример теста на производительность.
+        self.measure {
+            // Поместите сюда код, время которого вы хотите измерить.
+        }
+    }
+
+}
